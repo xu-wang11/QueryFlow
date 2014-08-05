9ba04cd4391acfb1032b9973788a056757a20460
xu-wang11 <wangxu.93@icloud.com>
2014-08-05 15:01:09 -0700

    add files

---
+++ cache/twig/99/ca/43c08e0aecd688034c49f3fcbdc5.php
@@ -0,0 +1,31 @@
+<?php
+
+/* oauth2/github.twig */
+class __TwigTemplate_99ca43c08e0aecd688034c49f3fcbdc5 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<a href=\"https://github.com/Luracast/Restler\"><img style=\"position: absolute; top: 0; right: 0; border: 0;z-index:100000\" src=\"https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png\" alt=\"Fork me on GitHub\"></a>";
+    }
+
+    public function getTemplateName()
+    {
+        return "oauth2/github.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  26 => 5,  21 => 2,  19 => 1,  76 => 29,  73 => 28,  64 => 30,  62 => 28,  56 => 24,  53 => 23,  48 => 21,  38 => 14,  34 => 13,  20 => 1,  60 => 28,  50 => 22,  31 => 4,  28 => 3,);
+    }
+}
